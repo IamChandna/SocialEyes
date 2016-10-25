d483d579b4fc7737e2f496fc95bef4d3821add46
Ilayaraja S <ilayaraja97@gmail.com>
2016-10-25 12:49:49 +0530

    added login module added bootstrap framework added slim framework

---
+++ SocialEyes/src/login/credentials.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * @author ilaya
+ * add credentials to connect to database
+ */
+$dbHost="172.16.40.26";
+$dbPort="5432";
+$dbName="student";
+$dbUser="student";
+$dbPass="student";
+$salt="!@#$%";
