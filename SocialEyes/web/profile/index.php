0f212048f9e7568bc587d1e677195f9c373fa340
Ilayaraja S <ilayaraja97@gmail.com>
2016-11-10 00:32:36 +0530

    profile/index.php taking ids

--- SocialEyes/web/profile/index.php
+++ SocialEyes/web/profile/index.php
@@ -4,7 +4,13 @@ if (! isset ( $_SESSION ['user'] )) {
 	header ( 'Location: login.php' );
 	exit ( 0 );
 }
-$root="../";
+$root = "../";
+
+$uri = ($_SERVER ['REQUEST_URI']);
+$exploaded = explode ( "/", $uri );
+$lastParamLoc = sizeof ( $exploaded );
+$id = $exploaded [$lastParamLoc - 1];
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,26 +24,28 @@ $root="../";
 </head>
 <body>
 		 <?php include "../php/topNavBar.php";?>
-		
-		 <div class="col-xs-8 col-xs-offset-1 col-sm-8  col-sm-offset-1 col-md-8  col-md-offset-1 col-lg-8 col-lg-offset-1" style="margin-top: 3.5em;">
-		 	<div class="main-content">
-		 		<?php 
-		 		include '../../src/postgres/query.php';
-		 		$o=new query();
-		 		?>
+		 <div
+		class="col-xs-8 col-xs-offset-1 col-sm-8  col-sm-offset-1 col-md-8  col-md-offset-1 col-lg-8 col-lg-offset-1"
+		style="margin-top: 3.5em;">
+		<div class="main-content">
+		 		<?php
+					include '../../src/postgres/query.php';
+					$o = new query ();
+					?>
 		 		<div class="coverpic">
-		 			<img alt="cover" src="../../src/postgres/<?php echo $o->getCoverpicForUid($_SESSION['user']['id']);?>">
-		 		</div>
-		 		
-		 	</div>
-	 	
-	 	</div>
+				<img alt="cover"
+					src="../../src/postgres/<?php echo $o->getCoverpicForUid($id);?>" />
+			</div>
 
-		<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-			<div class="sidebar-nav-fixed pull-right affix">
+		</div>
+
+	</div>
+
+	<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+		<div class="sidebar-nav-fixed pull-right affix">
 				<?php include "../php/chat.php"; ?>
 			</div>
-		</div>
-		
+	</div>
+
 </body>
 </html>
