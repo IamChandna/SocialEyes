fb198637a2e9bada97e8d0ed97f4ffe3e7b39bba
Ilayaraja S <ilayaraja97@gmail.com>
2016-11-19 12:12:28 +0530

    rendered chat block in home with issues

--- SocialEyes/src/chat/chatSearch.php
+++ SocialEyes/src/chat/chatSearch.php
@@ -12,7 +12,7 @@ foreach ($friends as $friend){
 	?>
 	<div class="row well"onclick="openChatWindow(<?php echo $_SESSION['user']['id'].",".$friend[1] ?>)">
 		<div class="col-xs-3 col-xs-offset-1 chat-profile-pic">
-			<img alt=":P" src="../../src/postgres/<?php echo $o->getPiclinkForPidFromGallery($friend[2]);?>">
+			<img alt=":P" src="../src/postgres/<?php echo $o->getPiclinkForPidFromGallery($friend[2]);?>">
 		</div>
 		<div class="col-xs-7 chat-usernames">
 			<label><?php echo $friend[0];?></label>
--- SocialEyes/web/home.php
+++ SocialEyes/web/home.php
@@ -7,7 +7,7 @@ if (! isset ( $_SESSION ['user'] )) {
 $root = "";
 include '../src/postgres/query.php';
 $o = new query ();
-$_SESSION['user']['root']=$root;
+$_SESSION ['user'] ['root'] = $root;
 ?>
 <!DOCTYPE html>
 <html>
@@ -55,11 +55,8 @@ $_SESSION['user']['root']=$root;
 		</div>
 	</div>
 
-	<div style="padding-left: 0px;"
-		class="col-xs-2 col-xs-offset-2 col-sm-2  col-sm-offset-2 col-md-2  col-md-offset-2 col-lg-2 col-lg-offset-2" >
-		<div class="sidebar-nav-fixed pull-right affix">
-				<?php include "php/chat.php"; ?>
-			</div>
+	<div style="padding-left: 0px;" class="col-xs-2 col-xs-offset-2 col-sm-2  col-sm-offset-2 col-md-2  col-md-offset-2 col-lg-2 col-lg-offset-2">
+		<?php include "php/chat.php"; ?>
 	</div>
 
 	<script>
