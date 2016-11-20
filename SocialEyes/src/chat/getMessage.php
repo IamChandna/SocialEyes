32290b7344d3b5188620ef558eb2b11628b61842
Jai_Pal_Singh <jai.pal.2013@gmail.com>
2016-11-20 18:18:17 +0530

    auto scroll added

---
+++ SocialEyes/src/chat/getMessage.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+include_once '../postgres/query.php';
+include_once '../status/periodElapsed.php';
+$o = new query ();
+$msgs=$o->getMsgUidTimeForConvid($_POST['convid']);
+
+foreach($msgs as $msg){
+	$p=periodElapsed(strtotime($msg[2]));
+	//$p=strtotime($msg[2]);
+	if($_SESSION["user"]["id"]==$msg[0])
+		echo "<div class='chat-messages sendermsg one'>".$msg[1]."</div>";
+	else
+		echo "<div class='chat-messages receivermsg one'>".$msg[1]."</div>";
+	echo "<div class='chat-messages one' style='font-size:10px;;'>".$p."</div>";
+}
